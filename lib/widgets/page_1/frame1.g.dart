e6e8fdbfa06ac73d87912925b8acfae8375457f9
Parabeac-Nest <admin@parabeac.com>
2022-07-26 22:36:47 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/widgets/page_1/frame1.g.dart
@@ -0,0 +1,48 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+//
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+
+import 'package:flutter/material.dart';
+
+class Frame1 extends StatefulWidget {
+  final BoxConstraints constraints;
+
+  const Frame1(
+    this.constraints, {
+    Key? key,
+  }) : super(key: key);
+  @override
+  _Frame1 createState() => _Frame1();
+}
+
+class _Frame1 extends State<Frame1> {
+  _Frame1();
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        decoration: BoxDecoration(),
+        child: Stack(children: [
+          Positioned(
+            left: 0,
+            width: 216.0,
+            top: 0,
+            height: 156.0,
+            child: Container(
+              height: 156.0,
+              width: 216.0,
+              decoration: BoxDecoration(
+                color: Color(0xff49ff0a),
+              ),
+            ),
+          ),
+        ]));
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+}
