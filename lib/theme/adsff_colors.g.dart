9efdb52031035360a838a2767cb842404e576dc7
Parabeac-Nest <admin@parabeac.com>
2022-07-27 18:15:28 +0000

    Auto-generated commit by Parabeac Nest

---
+++ lib/theme/adsff_colors.g.dart
@@ -0,0 +1,22 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+class AdsffColors {
+/// Parabeac Red Description
+static const Color parabeacRed = Color(0xffff0d0d);
+
+static const Color parabeacBlue = Color(0xff28e5ff);
+
+static const Color parabeacGreen = Color(0xff49ff0a);
+
+static const Color newColor = Color(0xff853d3d);
+
+static const Color hehehehe = Color(0xffffffff);
+
+static const Color testing = Color(0xffffffff);
+}
---
+++ lib/theme/adsff_theme.g.dart
@@ -0,0 +1,16 @@
+// *********************************************************************************
+// PARABEAC-GENERATED CODE. DO NOT MODIFY.
+// 
+// FOR MORE INFORMATION ON HOW TO USE PARABEAC, PLEASE VISIT docs.parabeac.com
+// *********************************************************************************
+    
+
+import 'package:flutter/material.dart';
+import 'package:adsff/theme/adsff_text_styles.g.dart';
+
+class AdsffTheme {
+
+static  ThemeData themeData = ThemeData(textTheme: textTheme,);
+
+static const TextTheme textTheme = TextTheme(headline1: AdsffTextStyles.headline1,headline2: AdsffTextStyles.headline2,);
+}
